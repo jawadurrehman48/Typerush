6445ca245f43d7580739ee9756f2bad94fb2de7e
Jawad Ur Rehman <jawadurRehman4884@gmail.com>
2026-01-02 05:18:35 +0000

    remove the nav bar from loing screen

--- src/components/auth/UserAuthForm.tsx
+++ src/components/auth/UserAuthForm.tsx
@@ -106,7 +106,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
           </span>
         </div>
       </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
+      <Button variant="outline" type="button" disabled={isLoading} className="w-full">
         {isLoading ? (
             <svg className="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>

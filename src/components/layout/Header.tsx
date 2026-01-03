ab82f40794fe7044665bdb60a13adbc2568fe1c4
Jawad Ur Rehman <jawadurRehman4884@gmail.com>
2026-01-03 08:49:39 +0000

    add some spacing in nav bar from left and right side

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 max-w-screen-2xl items-center">
+      <div className="container flex h-14 max-w-screen-2xl items-center px-6">
         <nav className="flex items-center space-x-6 text-sm font-medium">
             <Link href="/game" className="mr-6 flex items-center space-x-2">
             <Keyboard className="h-6 w-6 text-primary" />
